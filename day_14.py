99bd687b58004896ff3ef60a4f0d9c7d8f40021f
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-03-30 10:37:30 +0530

    Add files via upload

---
+++ day_14.py
@@ -0,0 +1,33 @@
+c="akhila"
+# print(c[ : : ])
+# print(c[0 : : 1])
+# print(c[ : : 2])
+# print(c[ : : 3])
+# print(c[ : : 4])
+# print(c[ : : 5])
+# print(c[ : : 6])
+# print(c[ : : 7])
+
+c="akhila"
+# print(c[ : : -1])
+# print(c[ : : -2])
+# print(c[ : : -3])
+# print(c[ : : -4])
+# print(c[ : : -5])
+# print(c[ : : -6])
+# print(c[ : : -7])
+
+c="akhila"
+# print(c[1 : :])
+# print(c[2 : :])
+# print(c[3 : :])
+# print(c[4 : :])
+# print(c[5 : :])
+# print(c[6 : :])
+# print(c[1: 6: ])
+# print(c[2: 5: ])
+# print(c[3: 4: ])
+# print(c[4: 3: ])
+# print(c[5: 2: ])
+# print(c[6: 1: ])
+# print(c[7: 0: ])
